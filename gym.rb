f566eea6549669707522549c2004a93237b0bc15
Butcher of Blaviken <makthecoder@gmail.com>
2016-10-02 17:18:22 -0400

    fix params

--- gym.rb
+++ gym.rb
@@ -6,12 +6,12 @@ end
 
 get '/webhook' do
     p params
-    hubmode = params['hub']['mode']
-    verify_token = params['hub']['verify_token']
+    hubmode = params['hub.mode']
+    verify_token = params['hub.verify_token']
     p hubmode
     p verify_token
     if hubmode == 'subscribe' and verify_token == ENV['VERIFY_TOKEN']
-        return [200, params['hub']['challenge'].to_i]
+        return [200, params['hub.challenge'].to_i]
     else
         return [403, 'Validation error']
     end
