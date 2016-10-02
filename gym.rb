d6b4c9aa1a5313d74c784cb425ab76ccb0853d42
Butcher of Blaviken <makthecoder@gmail.com>
2016-10-02 17:13:15 -0400

    add webhook enpoint

--- gym.rb
+++ gym.rb
@@ -4,6 +4,15 @@ get '/' do
     "Gymbot"
 end
 
-get '/api/v1' do
-    "Gymbot API"
+get '/webhook' do
+    p params
+    hubmode = params['hub']['mode']
+    verify_token = params['hub']['verify_token']
+    p hubmode
+    p verify_token
+    if hubmode == 'subscribe' and verify_token == ENV['VERIFY_TOKEN']
+        return [200, params['hub']['challenge'].to_i]
+    else
+        return [403, 'Validation error']
+    end
 end
